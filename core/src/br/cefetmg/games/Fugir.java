c08df7ee77a92a292d1097ddc8f6c02157f46207
samueloliveira492 <42243992+samueloliveira492@users.noreply.github.com>
2018-09-13 19:17:35 -0300

    Alteração movimentos

---
+++ core/src/br/cefetmg/games/Fugir.java
@@ -0,0 +1,44 @@
+package br.cefetmg.games.movement.behavior;
+
+import br.cefetmg.games.movement.AlgoritmoMovimentacao;
+import br.cefetmg.games.movement.Direcionamento;
+import br.cefetmg.games.movement.Pose;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Guia o agente de forma a fugir na direção contrária ao alvo.
+ *
+ * @author Flávio Coutinho <fegemo@cefetmg.br>
+ */
+public class Fugir extends AlgoritmoMovimentacao {
+
+    private static final char NOME = 'f';
+
+    public Fugir(float maxVelocidade) {
+        super(NOME);
+        this.maxVelocidade = maxVelocidade;
+    }
+
+    @Override
+    public Direcionamento guiar(Pose agente) {
+        Direcionamento output = new Direcionamento();
+        output.velocidade = new Vector3(agente.posicao).sub(alvo.getObjetivo());
+        output.velocidade.nor();
+        output.velocidade.scl(this.maxVelocidade);
+        agente.olharNaDirecaoDaVelocidade(output.velocidade);
+        // calcula que direção tomar (configura um objeto Direcionamento 
+        // e o retorna)
+        // ...
+        // super.alvo já contém a posição do alvo
+        // agente (parâmetro) é a pose do agente que estamos guiando
+        // ...
+        return output;
+    }
+
+    @Override
+    public int getTeclaParaAtivacao() {
+        return Keys.F;
+    }
+
+}
---
+++ core/src/br/cefetmg/games/Vagar.java
@@ -0,0 +1,46 @@
+package br.cefetmg.games.movement.behavior;
+
+import br.cefetmg.games.movement.AlgoritmoMovimentacao;
+import br.cefetmg.games.movement.Direcionamento;
+import br.cefetmg.games.movement.Pose;
+import com.badlogic.gdx.Input.Keys;
+
+/**
+ * Movimenta o agente em uma direção aleatória, vagando pelo cenário.
+ *
+ * @author Flávio Coutinho <fegemo@cefetmg.br>
+ */
+public class Vagar extends AlgoritmoMovimentacao {
+
+    private static final char NOME = 'w';
+    private float maxAngular = 30f;
+
+    public Vagar() {
+        super(NOME);
+    }
+
+    public Vagar(float tangencial, float angular) {
+        super(NOME);
+        maxVelocidade = tangencial;
+        maxAngular = angular;
+    }
+
+    @Override
+    public Direcionamento guiar(Pose agente) {
+        Direcionamento output = new Direcionamento();
+        output.velocidade = agente.getOrientacaoComoVetor().scl(this.maxVelocidade); 
+        output.rotacao = (Math.random()-Math.random())*this.maxAngular;
+        // calcula que direção tomar (configura um objeto Direcionamento 
+        // e o retorna)
+        // ...
+        // super.alvo já contém a posição do alvo
+        // agente (parâmetro) é a pose do agente que estamos guiando
+        // ...
+        return output;
+    }
+
+    @Override
+    public int getTeclaParaAtivacao() {
+        return Keys.W;
+    }
+}
